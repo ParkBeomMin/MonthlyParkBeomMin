29b7844f88bc248aa55a49d4dd43455cb79ad9e4
bmpark <user@userui-MacBookPro.local>
2022-09-26 23:56:28 +0900

    Header 생성

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <IconInstagram @click.prevent="goTo('instagram')" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import IconInstagram from "./icons/IconInstagram.vue";
+//TODO 컴포지션API로 빼서 관리하기
+const goTo = (des: String) => {
+  if (des == "instagram") {
+    //TODO 인스타링크로 보내기
+    console.log(1);
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: end;
+  padding: 8px;
+  border-bottom: 0.5px solid #b8b8c7;
+}
+</style>
